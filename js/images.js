b23914ff5960c584c5617273b2ba299e38057b3f
gitdenlev <levchenkoden02@gmail.com>
2023-12-01 01:53:53 +0200

    Reference

--- js/images.js
+++ js/images.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+// import axios from "axios";
 
-const API_KEY = "1J8PvZehiqvMmjUaiufjJScKNkSwq9eNr9dPZDLNA2U";
+// const API_KEY = "1J8PvZehiqvMmjUaiufjJScKNkSwq9eNr9dPZDLNA2U";
 
-async function getImageData() {
-  try {
-    const response = await axios.get(
-      `https://api.unsplash.com/search/photos?client_id=${API_KEY}&query=cat`
-    );
-    console.log(response);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching image data:", error);
-  }
-}
-getImageData()
+// async function getImageData() {
+//   try {
+//     const response = await axios.get(
+//       `https://api.unsplash.com/search/photos?client_id=${API_KEY}&query=cat`
+//     );
+//     console.log(response);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error fetching image data:", error);
+//   }
+// }
+// getImageData()
--- js/weather.js
+++ js/weather.js
@@ -1,16 +1,37 @@
 import axios from "axios";
+
 const API_KEY = "e104ae098f0f093ed9b4c57bb1bf107e";
+const inputCity = document.querySelector("#js-input");
+const mainTemperatureInfo = document.querySelector("h1");
 
+const getWeatherData = async () => {
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/weather?q=${inputCity.value}&appid=${API_KEY}`
+  );
+  console.log(response.data.main);
+  const toCelsius = (response.data.main.temp - 273.15).toFixed(0) // округляем до двух знаков после запятой
+  mainTemperatureInfo.textContent = toCelsius + " °C";
+};
 
+inputCity.addEventListener("change", () => {
+  getWeatherData();
+});
 
-async function getWeatherData() {
-    try {
-      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${API_KEY}`);
-      console.log(response);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching weather data:", error);
-    }
-  }
-  
-getWeatherData();
+//   function getWeatherData() {
+//   try {
+
+//     console.log(response.data.main.temp_min);
+//     inputCity.addEventListener("change", () => {
+//       const response = axios.get(
+//       `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${API_KEY}`
+//     );
+//     })
+//   } catch (error) {
+//     console.error("Error fetching weather data:", error);
+//   }
+//   // Convert from kelvins to celcius
+
+//   console.log((270.18-273).toFixed(2))
+// }
+
+// getWeatherData();
